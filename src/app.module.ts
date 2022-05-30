a589bc1b746a59dd18b8b2abe0eb9b4475c76ef2
Nidan23 <dev.sulkowski@gmail.com>
2022-05-30 17:25:32 +0200

    Added .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,22 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: 'schema.gql',
       driver: ApolloDriver,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'football',
-      password: 'team',
-      database: 'footballteam',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [`${__dirname}/model/*.model.js`, `${__dirname}/model/*.model.ts`],
       synchronize: false,
       keepConnectionAlive: true
